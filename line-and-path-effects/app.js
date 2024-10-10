5ea0c12e926caef56427d4efb682be45250933f7
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-10-10 22:40:10 +0200

    rainbow thunder effect with lines

--- line-and-path-effects/app.js
+++ line-and-path-effects/app.js
@@ -5,25 +5,78 @@ canvas.height = 900;
 
 // global settings
 ctx.lineWidth = 10;
-ctx.strokeStyle = "magenta";
 
 class Line {
   constructor(canvas) {
     this.canvas = canvas;
-    this.startX = Math.random() * this.canvas.width;
-    this.startY = Math.random() * this.canvas.height;
-    this.endX = Math.random() * this.canvas.width;
-    this.endY = Math.random() * this.canvas.height;
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
     this.lineWidth = Math.floor(Math.random() * 15 + 1);
+    this.hue = Math.floor(Math.random() * 360);
+    this.maxLength = Math.floor(Math.random() * 150 + 10);
+    this.speedX = Math.random() * 1 - 0.5;
+    this.speedY = 7;
+    this.lifeSpan = this.maxLength * 3;
+    this.timer = 0;
   }
+
   draw(context) {
+    context.strokeStyle = "hsl(" + this.hue + ",100%,50%)";
     context.lineWidth = this.lineWidth;
     context.beginPath();
-    context.moveTo(this.startX, this.startY);
-    context.lineTo(this.endX, this.endY);
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
     context.stroke();
+  }
+
+  update() {
+    this.timer++;
+    if (this.timer < this.lifeSpan) {
+      this.x += this.speedX + Math.random() * 20 - 10;
+      this.y += this.speedY + Math.random() * 20 - 10;
+      this.history.push({ x: this.x, y: this.y });
+
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length <= 1) {
+      this.reset();
+    } else {
+      this.history.shift();
+    }
+  }
+
+  reset() {
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
+    this.timer = 0;
   }
 }
 
-const line1 = new Line(canvas);
-line1.draw(ctx);
+const linesArray = [];
+
+const numberOfLines = 20;
+for (let i = 0; i < numberOfLines; i++) {
+  linesArray.push(new Line(canvas));
+}
+
+console.log(linesArray);
+
+linesArray.forEach((line) => line.draw(ctx));
+
+// animation
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // draw line
+  linesArray.forEach((line) => {
+    line.draw(ctx);
+    line.update();
+  });
+  // update line
+  requestAnimationFrame(animate);
+}
+animate();
