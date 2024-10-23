cc1971e4e516dd172c68648954ab623275f1743d
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2024-10-23 09:04:14 +0200

    animating particle systems

--- flow-fields/app.js
+++ flow-fields/app.js
@@ -8,10 +8,55 @@ canvas.height = window.innerHeight;
 console.log(ctx);
 ctx.fillStyle = "white";
 ctx.strokeStyle = "white";
-ctx.lineWidth = 100;
-ctx.lineCap = "round";
+ctx.lineWidth = 1;
 
-ctx.beginPath();
-ctx.moveTo(100, 200);
-ctx.lineTo(400, 500);
-ctx.stroke();
+class Particle {
+  constructor(effect) {
+    this.effect = effect;
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+    this.speedX = Math.random() * 5 - 2.5;
+    this.speedY = Math.random() * 5 - 2.5;
+  }
+  draw(context) {
+    context.fillRect(this.x, this.y, 50, 36);
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+}
+
+class Effect {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.particles = [];
+    this.numberOfParticles = 50;
+    this.init();
+  }
+  init() {
+    // create particles
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
+    this.particles.push(new Particle(this));
+  }
+  render(context) {
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+      particle.update();
+    });
+  }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+console.log(effect);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  effect.render(ctx);
+  requestAnimationFrame(animate);
+}
+
+animate();
